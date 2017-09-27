8a76f9e3045d7b5fc8b44b923b7e06a3c9f86a39
zmwilliam <willcxa@gmail.com>
2017-09-27 18:45:39 -0300

    Downloading drivers to os temp folder

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const childProcess = require('child_process');
+const fs = require('fs');
+const http = require('http');
+const os = require('os');
 
 class Liquibase {
 	constructor(params = {}) {
 		const defaultParams = {
-			liquibase: 'lib/liquibase-core-3.5.3.jar',
+			liquibase: `${os.tmpdir()}/liquibase-core.jar`,
 			driver: 'org.postgresql.Driver',
-			classpath: 'lib/postgresql-9.4-1201.jdbc4.jar'
+			classpath: `${os.tmpdir()}/postgresql-jdbc.jar`
 		};
 		this.params = Object.assign({}, defaultParams, params);
 	}
@@ -38,8 +41,37 @@ class Liquibase {
 		return promise;
 	}
 
-	run(action = 'update', params = '') {
+	async run(action = 'update', params = '') {
+		await this.downloadDrivers();
 		return this.exec(`${this.command} ${action} ${params}`);
+	}
+
+	async downloadDrivers() {
+		const liquibaseURL = 'http://central.maven.org/maven2/org/liquibase/liquibase-core/3.5.3/liquibase-core-3.5.3.jar';
+		const psqlJdbcURL = 'http://central.maven.org/maven2/org/postgresql/postgresql/42.1.4/postgresql-42.1.4.jar';
+
+		if (!fs.existsSync(this.params.liquibase)) {
+			return await this.download(liquibaseURL, this.params.liquibase);
+		}
+
+		if (!fs.existsSync(this.params.classpath)) {
+			return await this.download(psqlJdbcURL, this.params.classpath);
+		}
+	}
+
+	download(url, dest) {
+		return new Promise((resolve, reject) => {
+			var file = fs.createWriteStream(dest);
+			http.get(url, response => {
+				response.pipe(file);
+				file.on('finish', () => {
+					file.close(resolve);
+				});
+			}).on('error', err => {
+				fs.unlinkSync(dest);
+				reject(err);
+			});
+		});
 	}
 }
 
